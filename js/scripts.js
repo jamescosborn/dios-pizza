ab4e720070a077d56449b81e11b2306b2d111a97
James Osborn <jamescarlosborn@gmail.com>
2017-09-29 11:14:32 -0700

    correct variable names with orderInfo, display orderInfo in an almost finished state (without price).

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,27 @@ function Pizza(toppings, size) {
   this.pizzaSize = size;
 }
 Pizza.prototype.orderInfo= function () {
-  return "So that's a" + this.pizzaSize + this.pizzaToppings + ". Yeah, we'll get right on that.. See you in 4 to 5 hours!" ;
+  return "So that's a " + this.pizzaSize + this.pizzaToppings + ". Yeah, we'll get right on that.. See you in 4 to 5 hours!" ;
 }
 
-Ticket.prototype.getPrice = function () {
+Pizza.prototype.getPrice = function () {
   var standardPrice = 10;
   if (this.pizzaToppings.match(/[V]\w+/g)) {
     alert("You're winner!")
   }
+  if (this.pizzaToppings.match(/[C]\w+/g)) {
+    alert("Cheesey!")
+  }
+  if (this.pizzaToppings.match(/[Y]\w+/g)) {
+    alert("Yep yep yep!")
+  }
+  if (this.pizzaToppings.match(/[R]\w+/g)) {
+    alert("Go have a feast at lunch!")
+  }
+  else {
+    alert("Flyin Hawaiian huh?")
+  };
+};
 //   else if (this.movieTime <= 4) {
 //     price /= 2;
 //   }
@@ -26,7 +39,7 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var inputtedToppings = $("#pizzaToppings").val();
-    var inputtedSize = $("#movieTime").val();
+    var inputtedSize = $("#pizzaSize").val();
     var pizza = new Pizza (inputtedToppings, inputtedSize);
     $(".output").text(pizza.orderInfo());
     // $(".output").text("<p>" + ticket.allStuff() + "</p>");
